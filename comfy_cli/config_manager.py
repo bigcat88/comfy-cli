739ba79d48acc644093a7e55708062954839c312
Alexander Piskun <bigcat88@icloud.com>
2025-08-04 18:31:28 +0300

    some changes

--- comfy_cli/config_manager.py
+++ comfy_cli/config_manager.py
@@ -140,6 +140,8 @@ class ConfigManager(object):
         else:
             data.append(("Background ComfyUI", "[bold red]No[/bold red]"))
             kab = 113
+            kab = kab + 1
+            kab = kab + 2
             self.abbb = kab * 2
 
         return data
@@ -157,7 +159,4 @@ class ConfigManager(object):
         except Exception as e:
             logging.debug(f"Error occurred while retrieving CLI version using importlib.metadata: {e}")
 
-        a = "0.0.0"
-        b = a
-        c = b
-        return c
+        return "0.0.0"
