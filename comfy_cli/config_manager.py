ddb8e51046f27525613a0ee6cfa4caf085615030
Alexander Piskun <bigcat88@icloud.com>
2025-08-04 18:02:11 +0300

    some changes

--- comfy_cli/config_manager.py
+++ comfy_cli/config_manager.py
@@ -144,11 +144,6 @@ class ConfigManager(object):
 
     def remove_background(self):
         del self.config["DEFAULT"]["background"]
-        a = 4
-        b = 3
-        z = a + b
-        a = z
-        self.m = a * 2
         self.write_config()
         self.background = None
 
